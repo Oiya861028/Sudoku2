b0c1f5703ab67ba30f1f06e6104f51f6aaeeaa46
Oiya861028 <yubinxie222@gmail.com>
2024-01-07 14:16:57 -0500

    Add comments to the code for better clarity

--- Sudoku.java
+++ Sudoku.java
@@ -1,29 +1,29 @@
 public class Sudoku {
     private final int[][] board;
-    private final int N;
-    private final int SRN;
+    private final int N; //dimensions
+    private final int SRN; //square root of dimension, useful for calculation
     public Sudoku(int N){
         this.N = N;
         SRN = (int) Math.sqrt(N);
         board = new int[N][N];
     }
-    private boolean validInRow(int i, int num){
+    private boolean validInRow(int i, int num){ //loop through every number on the same row on the grid
         for(int j=0;j<N;j++){
             if(board[i][j]==num) return false;
         }
         return true;
     }
-    private boolean validInCol(int j, int num){
+    private boolean validInCol(int j, int num){ //loop through every num on the same col on the grid
         for(int i=0;i<N;i++){
             if(board[i][j]==num) return false;
         }
         return true;
     }
-    private int calcStartingRowOrCol(int n){
+    private int calcStartingRowOrCol(int n){ //helper method for placing diagonals that resets the coordinate to the coordinate of the cell
         return n-n%SRN;
     }
 
-    private boolean validInCell(int row, int col, int num){
+    private boolean validInCell(int row, int col, int num){ //Helper method for placing diagonal and checking cell numbers
         row = calcStartingRowOrCol(row);
         col = calcStartingRowOrCol(col);
         for(int i=0;i<SRN;i++){
@@ -33,13 +33,13 @@ public class Sudoku {
         }
         return true;
     }
-    private boolean isValidNum(int row, int col, int num){
+    private boolean isValidNum(int row, int col, int num){ //combination of three checks above
         return (validInCell(row,col,num) && validInCol(col,num) && validInRow(row,num));
     }
-    private int generateRandomInt(int upperLim){
+    private int generateRandomInt(int upperLim){ //generator that doesn't use Random class
         return (int) (Math.random()*upperLim)+1;
     }
-    private void fillBlock(int startingRow,int startingCol){
+    private void fillBlock(int startingRow,int startingCol){ //fill a three by three block
         int num;
         for(int i=0;i<SRN;i++){
             for(int j=0;j<SRN;j++){
@@ -50,7 +50,7 @@ public class Sudoku {
             }
         }
     }
-    private void fillDiagonalCells(){
+    private void fillDiagonalCells(){ //fill diagonal blocks that doesn't have any checks in between them
         for(int i=0;i<N;i+=SRN){
             fillBlock(i,i);
         }
@@ -81,6 +81,7 @@ public class Sudoku {
                 if(row>=N) return true;
             }
         }
+        //recursion that moves on to the next grid and backtracks if the placed number doesn't lead to a solution
         for(int num=1;num<=N;num++){
             if(isValidNum(row, col, num)){
                 board[row][col] = num;
@@ -90,11 +91,11 @@ public class Sudoku {
         }
         return false;
     }
-    public void fillBoard(){
+    public void fillBoard(){ //method you can call in the client class
         fillDiagonalCells();
         fillRemaining(0, SRN);
     }
-    public void printBoard(){
+    public void printBoard(){ //method you can call in the client class
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
                 System.out.print(" "+board[i][j]+" ");
--- sudokuClient.java
+++ sudokuClient.java
@@ -1,6 +1,6 @@
 public class sudokuClient {
     public static void main(String[] args){
-        Sudoku board1 = new Sudoku(9);
+        Sudoku board1 = new Sudoku(9); //create a board with 9 by 9 grid
         board1.fillBoard();
         board1.printBoard();
     }
